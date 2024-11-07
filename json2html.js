ce6bc1ecec5ccc2dc3d916de9ac8dc763ec696e5
harshaVarnam <harshavarnam@gmail.com>
2024-11-07 19:26:40 +0530

    Add files via upload

---
+++ json2html.js
@@ -0,0 +1,23 @@
+
+export default function json2html(data) {
+    let html = '<table data-user="harshavardhanreddyvarnam@gmail.com"><thead><tr>';
+
+    const headers = Array.from(new Set(data.flatMap(Object.keys)));
+
+    headers.forEach(header => {
+      html += `<th>${header}</th>`;
+    });
+    html += "</tr></thead><tbody>";
+
+    data.forEach(row => {
+      html += "<tr>";
+      headers.forEach(header => {
+        html += `<td>${row[header] || ''}</td>`; 
+      });
+      html += "</tr>";
+    });
+
+    html += "</tbody></table>";
+    return html;
+}
+
